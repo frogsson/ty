d2e6465be1acf58abdd2b1c93f7a08bbabdc7953
kim <taengoe@gmail.com>
2019-06-09 05:25:59 +0200

    fetch stuff

---
+++ httpbin.py
@@ -0,0 +1,46 @@
+import urllib.request
+import logging
+
+
+class Fetch:
+    """wrapper for urllib.request with exceptions"""
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) \
+                       AppleWebKit/601.5.17 (KHTML, like Gecko) Version/9.1 Safari/601.5.17'
+    }
+    errors = []
+
+    def __init__(self, url):
+        self.logger = logging.getLogger('Fetch')
+        self.url = url
+        self._info, self._body = self.urlopen()
+
+    def urlopen(self):
+        try:
+            req = urllib.request.Request(self.url, headers=self.headers)
+            with urllib.request.urlopen(req) as req:
+                body = req.read()
+                info = req.info()
+                self.logger.debug('returning request [HTTP status code: %s]', req.getcode())
+            return info, body
+        except Exception as err:
+            err = "{} [{}]".format(self.url, err)
+            print(err)
+            self.errors.append(err)
+            return None
+
+    def body(self):
+        return self._body
+
+    def info(self):
+        return self._info
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(name)s: %(message)s')
+    test = Fetch("https://httpbin.org/get")
+    logging.debug(test.info())
+    logging.debug(test.body())
+    logging.debug('errors: %s', Fetch.errors)
