46374f78264e41a3d4fbaec15c3e37955061258f
kim <taengoe@gmail.com>
2019-06-09 05:28:56 +0200

    added more options to defaults

--- config_def.py
+++ config_def.py
@@ -3,3 +3,13 @@ organize = False
 
 # number simultaneous downloads
 threads = 6
+
+# default download directory
+# downloads to current directory if none is given.
+directory = ''
+
+# default pages
+pages = []
+
+# default title filter
+title_filter = []
